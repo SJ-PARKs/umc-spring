5447a42b00280a8cc683cfeaf599007e74c12d07
SJ <sungjunp1208@gmail.com>
2024-06-17 02:41:32 +0900

    특정가게 미션 조회 api

--- src/main/java/umc/spring/converter/StoreConverter.java
+++ src/main/java/umc/spring/converter/StoreConverter.java
@@ -81,4 +81,26 @@ public class StoreConverter {
                 .createdAt(LocalDateTime.now())
                 .build();
     }
+    public static StoreResponseDTO.MissionPreViewDTO missionPreViewDTO(Mission mission) {
+        return StoreResponseDTO.MissionPreViewDTO.builder()
+                .missionSpec(mission.getMissionSpec())
+                .reward(mission.getReward())
+                .deadline(mission.getDeadline())
+                .createdAt(mission.getCreatedAt().toLocalDate())
+                .build();
+    }
+
+    public static StoreResponseDTO.MissionPreViewListDTO missionPreViewListDTO(Page<Mission> missionList) {
+        List<StoreResponseDTO.MissionPreViewDTO> missionPreViewDTOList = missionList.stream()
+                .map(StoreConverter::missionPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.MissionPreViewListDTO.builder()
+                .isLast(missionList.isLast())
+                .isFirst(missionList.isFirst())
+                .totalPage(missionList.getTotalPages())
+                .totalElements(missionList.getTotalElements())
+                .listSize(missionPreViewDTOList.size())
+                .missionList(missionPreViewDTOList)
+                .build();
+    }
 }
